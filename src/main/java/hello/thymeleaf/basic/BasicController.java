e7a8d720a75c83945d6c629494d482ed87a6a12c
ufonetcom <ufozx@naver.com>
2022-03-09 18:56:16 +0900

    주석

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -103,10 +103,16 @@ public class BasicController {
         return "basic/each";
     }
 
-    @GetMapping("condition")
+    @GetMapping("/condition")
     public String condition(Model model) {
         addUsers(model);
         return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
     }
 
 
